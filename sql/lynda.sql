31bb394d9b31f3d6586874c8942d516826c1b478
Victoria Hooker <vhooker@cnm.edu>
2016-01-20 09:18:26 -0700

    Added the Drop Tables mySQL

---
+++ sql/lynda.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS viewed;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	email VARCHAR (128) NOT NULL,
+	phone VARCHAR (32),
+	UNIQUE (email),
+	PRIMARY KEY (profileId)
+);
+
+CREATE TABLE courses (
+	coursesId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
+	courseContent VARCHAR (140) NOT NULL,
+	courseDate DATETIME NOT NULL,
+	INDEX(profileId),
+	FOREIGN KEY(profileId) REFERENCES profile(profileId),
+	PRIMARY KEY (coursesId)
+);
+
+CREATE TABLE viewed (
+	profileId INT UNSIGNED NOT NULL,
+	coursesId INT UNSIGNED NOT NULL,
+	viewedID INT UNSIGNED NOT NULL,
+	viewedDate DATETIME NOT NULL,
+	shared VARCHAR (140)NOT NULL,
+	INDEX(profileId),
+	INDEX(coursesId),
+	FOREIGN KEY(profileId) REFERENCES  profile(profileId),
+	FOREIGN KEY(coursesId) REFERENCES courses(coursesId),
+	PRIMARY KEY(profileId, viewedID)
+);
