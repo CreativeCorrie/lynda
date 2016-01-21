87fc99f7de54f8fd8d1d71b429b08df6502ea1f5
Victoria Hooker <vhooker@cnm.edu>
2016-01-21 09:30:36 -0700

    Created Document page

---
+++ php/classes/classes-profileId.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * ProfileId, a tool to heal a player
+ *
+ * A ProfileId is the generic name for the users profile
+ * A person must provide this information to be able to utilize the Lynda.com resources
+ *
+ * @author Corrie Hooker <creativecorrie@gmail.com>
+ **/
+
+class ProfileId {
+	/**
+	 * id for user; this is the primary key
+	 * @var int $profileId
+	 **/
+	private $profileId;
+	/**
+	 * email, for communication purposes
+	 * @var int $email
+	 **/
+	private $email;
+	/**
+	 * name of user
+	 * @var int $name
+	 **/
+	private $name;
+	/**
+	/**
+	 * accessor method for profile id
+	 *
+	 * @return int value of profile id
+	 **/
+	public function getProfileId() {
+		return($this->profileId);
+	}
+
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param int $newProfileId new value of profile id
+	 * @throws InvalidArgumentException if profile id is not an integer
+	 * @throws RangeException if profile id is negative
+	 **/
+	public function setProfileId($newProfileId) {
+		//first, apply the filter to the input
+		$newProfileId=filter_var($newProfileId, FILTER_VALIDATE_INT);
+			//if filter_var rejects the new id, throw and Exception
+		if($newProfileId === false) {
+			throw(new InvalidArgumentException("profile id is not an integer"));
+		}
+		 //if the profile id is out of range, throw an exception
+		if($newProfileId <= 0) {
+			throw(new RangeException("profile if must be and email address"));
+		}
+		//finally, if we got here, we know it's a valid id  save it to the object
+		$this->profileId = $newProfileId;
+	}
+}
